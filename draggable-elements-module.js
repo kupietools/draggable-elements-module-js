11a0da3dbc2bcd0b96bbca105e16ead3ee90e52e
Michael Kupietz <26891995+kupietools@users.noreply.github.com>
2025-08-16 01:53:20 -0400

    Update draggable-elements-module.js

--- draggable-elements-module.js
+++ draggable-elements-module.js
@@ -111,7 +111,7 @@ class draggable_elements {
 
   setupDraggables() {
     //console.log('Setting up draggables:', this.config);
-	  const cssUrl = new URL(./draggable-elements.css, import.meta.url);
+	  const cssUrl = new URL("./draggable-elements.css", import.meta.url);
 	  const link = document.createElement('link');
 	  link.rel = 'stylesheet';
       link.href = cssUrl.href;
